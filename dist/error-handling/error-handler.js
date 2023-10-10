0c7055dc0611296db31fced03628496233ade954
amagalla <amagalla24@gmail.com>
2023-10-10 14:43:10 -0700

    removed dist

--- dist/error-handling/error-handler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => {
-    let status = err.status || 500;
-    let message = err.message || "Internal Server Error";
-    res.status(status).json({ error: message });
-};
-exports.default = errorHandler;
